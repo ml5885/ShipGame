a18527f54b4fb9cf261b742d571879ae022ca068
Michael Li <ml5885@pleasantonusd.net>
2020-09-16 19:24:08 -0700

    Made changes

---
+++ Waves/Assets/Scripts/BoatHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoatHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int points = 0;
+    public int currentHealth;
+    public HealthBar healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.setMaxHealth(maxHealth);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentHealth == 0)
+        {
+            Destroy(gameObject);
+            Time.timeScale = 0.0f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Enemy")
+        {
+            Debug.Log("hit enemy");
+            currentHealth = currentHealth - 10;
+            healthBar.setHealth(currentHealth);
+        }
+        else if(col.tag == "Coins")
+        {
+            points += 1;
+            Debug.Log(points);
+        }
+    }
+}
---
+++ Waves/Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public BoatHealth BoatHealth;
+    public TMPro.TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+    	scoreText = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = BoatHealth.points.ToString();
+    }
+}
